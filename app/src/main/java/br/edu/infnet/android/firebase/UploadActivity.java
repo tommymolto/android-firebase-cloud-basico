b484b09fcfd9f02a69f0445275434c9187349ad4
paulo marinho <paulo.marinho@personates.com>
2017-08-22 09:55:22 -0300

    first commit

---
+++ app/src/main/java/br/edu/infnet/android/firebase/UploadActivity.java
@@ -0,0 +1,112 @@
+package br.edu.infnet.android.firebase;
+
+import android.content.Intent;
+import android.content.res.AssetManager;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.view.View;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+
+public class UploadActivity extends RootActivity {
+
+    private StorageReference storageReference;
+    private FirebaseStorage storage;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_upload);
+        storage = FirebaseStorage.getInstance();
+        storageReference = storage.getReferenceFromUrl("gs://gec-2017.appspot.com/").child("firebase.png");
+
+        View btnUploadImage = findViewById(R.id.btn_upload_image);
+        btnUploadImage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                AssetManager assetManager = UploadActivity.this.getAssets();
+                InputStream istr;
+                Bitmap bitmap;
+                try {
+                    //get bitmap from PNG file in assets folder
+                    istr = assetManager.open("firebase.png");
+                    bitmap = BitmapFactory.decodeStream(istr);
+
+                    //decode to byte output stream
+                    ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+                    bitmap.compress(Bitmap.CompressFormat.PNG, 100, outputStream);
+                    byte[] data = outputStream.toByteArray();
+
+                    //Upload to firebase
+                    showProgressDialog("Subindo Bitmap", " com SPOILERS...");
+                    UploadTask uploadTask = storageReference.putBytes(data);
+                    uploadTask.addOnFailureListener(new OnFailureListener() {
+                        @Override
+                        public void onFailure(@NonNull Exception exception) {
+                            exception.printStackTrace();
+                            dismissProgressDialog();
+                            Toast.makeText(UploadActivity.this, "Upload Falhou!", Toast.LENGTH_SHORT).show();
+                        }
+                    }).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                        @Override
+                        public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                            dismissProgressDialog();
+                            Toast.makeText(UploadActivity.this, "Upload com sucesso!", Toast.LENGTH_SHORT).show();
+                        }
+                    });
+
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+
+        View btnUploadFile = findViewById(R.id.btn_upload_file);
+        btnUploadFile.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                storageReference = storage.getReferenceFromUrl("gs://gec-2017.appspot.com/").child("test_upload.txt");
+
+                //Upload input stream to Firebase
+                showProgressDialog("Upload File", "Subundo arquivo texto com SPOILERS...");
+                InputStream stream = getResources().openRawResource(R.raw.test);
+                UploadTask uploadTask = storageReference.putStream(stream);
+                uploadTask.addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception exception) {
+                        exception.printStackTrace();
+                        dismissProgressDialog();
+                        Toast.makeText(UploadActivity.this, "Upload Falhou!", Toast.LENGTH_SHORT).show();
+                    }
+                }).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                    @Override
+                    public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                        dismissProgressDialog();
+                        Toast.makeText(UploadActivity.this, "Upload com Sucesso!", Toast.LENGTH_SHORT).show();
+                    }
+                });
+            }
+        });
+
+        View btnDownload = findViewById(R.id.btn_download);
+        btnDownload.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(UploadActivity.this, DownloadActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
